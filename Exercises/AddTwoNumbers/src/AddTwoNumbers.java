f714108a8abe445077d656f9462fff14df9e02cf
Artur Bomtempo Colen <arturbcolen@gmail.com>
2024-08-17 23:59:37 -0300

    feat: addition of the first exercise

---
+++ Exercises/AddTwoNumbers/src/AddTwoNumbers.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class AddTwoNumbers {
+	public static Scanner sc = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		int number1, number2, sum;
+		
+		System.out.println("Digite o primeiro número:");
+		number1 = sc.nextInt();
+		
+		System.out.println("Digite o segundo número:");
+		number2 = sc.nextInt();
+		
+		sum = number1 + number2;
+		
+		System.out.println("Resultado: " + sum);
+	}
+}

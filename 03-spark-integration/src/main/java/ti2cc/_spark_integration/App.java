85c58dc74f4deec031501843ba8487243ec4c296
Artur Bomtempo Colen <arturbcolen@gmail.com>
2024-09-20 01:58:27 -0300

    feat: addition of evaluation exercise 03

---
+++ 03-spark-integration/src/main/java/ti2cc/_spark_integration/App.java
@@ -0,0 +1,13 @@
+package ti2cc._spark_integration;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
